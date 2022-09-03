14edd2fc9a60e99aba974936cd58ad17108fa2e4
CorreyL <correylim@gmail.com>
2022-09-02 21:52:27 -0700

    Implement logic to handle input of text
    - Add a React state to hold the list of applicable options based on the
      guest's input
    - Filter the applicable options based on matching substrings of the user
      inputs, ensuring that both the input and compared string is both
      lower-cased to ensure casing of the input does not matter

--- src/components/NameInput/NameInput.js
+++ src/components/NameInput/NameInput.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 
 const NameInput = ({
@@ -8,12 +8,26 @@ const NameInput = ({
   // Flatten the array of arrays into an array of strings
   const searchableGuestList = [].concat.apply([], Object.values(guestList));
 
+  const [searchMatches, setSearchMatches] = useState([]);
+
+  const onChange = (event) => {
+    const { value } = event.target;
+    if (!value) {
+      setSearchMatches([]);
+      return;
+    }
+    setSearchMatches(searchableGuestList.filter(name => (
+      name.toLowerCase().includes(value.toLowerCase())
+    )));
+  };
+
   return (
     <TextField
       label="Your Name"
       variant="outlined"
       color="success"
       inputRef={textInputRef}
+      onChange={onChange}
       focused
     />
   );
