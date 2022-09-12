2c01923e45b50002713a963448af31228421422e
CorreyL <correylim@gmail.com>
2022-09-11 19:49:09 -0700

    Add basic rendering of tables

--- src/components/TableCanvas/TableCanvas.js
+++ src/components/TableCanvas/TableCanvas.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import './TableCanvas.css';
 
 const TableCanvas = ({
   nameSelected
@@ -6,9 +7,28 @@ const TableCanvas = ({
   if (!nameSelected) {
     return null;
   }
+
   return (
-    <div>
-      {nameSelected}
+    <div className="parent">
+      <div className="head-row">
+        <div className="horizontal-rectangle">1</div>
+      </div>
+      <div className="guest-row">
+        <div className="vertical-rectangle">2</div>
+        <div className="vertical-rectangle">3</div>
+      </div>
+      <div className="guest-row">
+        <div className="vertical-rectangle">4</div>
+        <div className="vertical-rectangle">5</div>
+      </div>
+      <div className="guest-row">
+        <div className="vertical-rectangle">6</div>
+        <div className="vertical-rectangle">7</div>
+      </div>
+      <div className="guest-row">
+        <div className="vertical-rectangle">8</div>
+        <div className="vertical-rectangle">9</div>
+      </div>
     </div>
   );
 }
